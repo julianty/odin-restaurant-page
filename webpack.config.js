95d24efbcff965954d6a355431bdbde62ccf8918
julianty <alexanderjulianty@gmail.com>
2022-06-01 10:57:34 -0700

    Adds webserver functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    // publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  devServer: {
+    static: './dist',
   },
 };
